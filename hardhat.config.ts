4714c3f3da4e9b81e7a6c99daf540ee63aa11eee
Gianfabio Pezzolla <gianfabio.pezzolla@gmail.com>
2022-04-02 20:13:37 +0200

    demo completed

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,11 +5,20 @@ import "@typechain/hardhat";
 import "@nomiclabs/hardhat-ethers";
 import "tsconfig-paths/register";
 
+const ALCHEMY_API_KEY = "YOUR_API_KEY";
+const ROPSTEN_PRIVATE_KEY = "YOUR_PRIVATE_KEY";
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 const config: HardhatUserConfig = {
   solidity: "0.7.3",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;
